f9f6e38006313f216b01c7a74730a7e13d09874c
M Bilal <bilalsikander87@gmail.com>
2025-11-04 22:14:44 +0500

    Add button

--- tailwind/src/App.jsx
+++ tailwind/src/App.jsx
@@ -4,10 +4,10 @@ import logo from "./assets/OIP.webp"
 function App() {
   return (
     <div className='bg-slate-900 grid place-items-center h-screen'>
-      <div>
+      <div className='text-center'>
         <div className='p-5 max-w-sm mx-auto bg-white rounded-xl shadow-xl flex items-center space-x-3'>
           <div>
-            <img src={logo} alt="" className='h-16 w-16'/>
+            <img src={logo} alt="" className='h-16 w-16' />
           </div>
           <div>
             <div className='text-2xl font-medium text-black'>
@@ -15,6 +15,10 @@ function App() {
               <p className='text-slate-500 text-base'>By Bilal Sikander</p>
             </div>
           </div>
+        </div>
+        <button className='bg-sky-500 w-full mt-2 text-white p-2 rounded-xl hover:bg-white hover:text-black cursor-pointer'>Buy Now</button>
+        <div className='mt-2'>
+          <p className='text-white sm:text-red-500 md:text-green-500 lg:text-blue-500'>Lorem, ipsum dolor.</p>
         </div>
       </div>
     </div>
